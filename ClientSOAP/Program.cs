0f41ef66650456e99b307e9b56b3c6b563c75096
Lyl55 <88458345+Lyl55@users.noreply.github.com>
2023-07-25 22:59:30 +0400

    first commit

---
+++ ClientSOAP/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClientSOAP.Banguat;
+
+namespace ClientSOAP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Banguat.TipoCambio client = new TipoCambio();
+            var result = client.TipoCambioDia();
+            foreach (var item in result.CambioDolar)
+            {
+                Console.WriteLine(item.referencia);
+            }
+            Console.WriteLine(result.CambioDolar.First().fecha);
+            Console.WriteLine(result.CambioDolar.First().referencia);
+        }
+    }
+}
